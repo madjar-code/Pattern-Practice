4379290fae45f7caa12f3243e0f5554e9e343167
Madjar Ivan <evanmadjar@gmail.com>
2023-05-21 14:37:21 +0300

    feat: Create phone message class
    I have created a class for sending phone message.
    The class inherits from the message base class.

--- The Factory Method/Mailing/phone_mailing.py
+++ The Factory Method/Mailing/phone_mailing.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 from typing import (
     NamedTuple,
     Optional,
@@ -14,3 +16,24 @@ users = [
     User(name="alex", phone="+222222222"),
     User(name="george", phone="+333333333")
 ]
+
+
+class Message(ABC):
+    @abstractmethod
+    def send_message(self):
+        pass
+
+
+class PhoneMessage(Message):
+    def __init__(self, text: str, recipient: User) -> None:
+        self.message_text: str = text
+        self.recipient: User = recipient
+
+    def send_message(self) -> None:
+        """Send message to phone"""
+        try:
+            # code to message sending
+            print('Phone message sent')
+        except:
+            print('Problems sending a phone message')
+
